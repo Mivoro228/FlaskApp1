b49edf786f91799eb4f07566bb4e1cb3e8373c24
Mivoro <voronov.m.7@gmail.com>
2025-09-30 20:32:19 +0500

    версия на 30.09.25

--- FlaskPractice/app/routes.py
+++ FlaskPractice/app/routes.py
@@ -1,9 +1,33 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 #-*- coding: utf-8 -*-
 from app import app
+from app.forms import LoginForm
+#...
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username' : 'Miguel' } 
-    return  render_template('index.html', title ='Home', user=user)
+    posts = [
+        {
+            'author': {'username' : 'Miha'},
+            'body':'Big sus in New-York City'
+        },
+        {
+            'author': {'username': 'Putin'},
+            'body': 'Geopolitics in Russia'
+        },
+        {
+            'author':{'username': 'king-kong'},
+            'body': 'GRaaaaa'
+        }
+    ]
+    return  render_template('index.html', title ='Home', user=user, posts = posts)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form =  LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title ='Sign In', form=form)
+    
