8c50d9ca76179adf6b57c52b6bb2976e36d27902
Mivoro <voronov.m.7@gmail.com>
2025-10-02 19:24:25 +0500

    добавлены множественные изменения, поддержка logpass входа, logout, обработка исключений

--- FlaskPractice/app/routes.py
+++ FlaskPractice/app/routes.py
@@ -1,11 +1,18 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 #-*- coding: utf-8 -*-
 from app import app
+import sqlalchemy as sa
 from app.forms import LoginForm
+from flask_login import current_user, login_user, logout_user, login_required
+from urllib.parse import urlsplit
+from app import db
+from app.models import User
+
 #...
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     user = {'username' : 'Miguel' } 
     posts = [
@@ -25,9 +32,23 @@ def index():
     return  render_template('index.html', title ='Home', user=user, posts = posts)
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form =  LoginForm()
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data))
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or urlsplit(next_page).netloc != '':
+            next_page = url_for('index')
+            return redirect(next_page)
         return redirect(url_for('index'))
     return render_template('login.html', title ='Sign In', form=form)
-    
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
