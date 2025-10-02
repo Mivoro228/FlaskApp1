8d38388a0ca55a7e4ccc1d9432e83db7a963128c
Mivoro <voronov.m.7@gmail.com>
2025-10-02 09:44:36 +0500

    обнулены миграции бд, добавлен метод хеша паролей

--- FlaskPractice/app/models.py
+++ FlaskPractice/app/models.py
@@ -3,13 +3,19 @@ from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     id : so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(128), index=True, unique=True)
-    password_hash: so.Mapped[[Optional[str]]] = so.mapped_column(sa.String(256))
+    password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
     posts: so.WriteOnlyMapped['Post'] = so.relationship(back_populates='author')
+    def set_password(self, password):
+        self.password_hash = generate_password_hash
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
     def __repr__(self):
         return '<User {}>'.format(self.username)
     
