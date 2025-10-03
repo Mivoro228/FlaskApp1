ce08b83a127af74a05141b6a1029e2531281cc27
Mivoro <voronov.m.7@gmail.com>
2025-10-03 17:54:15 +0500

    версия с авторизацией пользователя

--- FlaskPractice/app/routes.py
+++ FlaskPractice/app/routes.py
@@ -2,7 +2,7 @@ from flask import render_template, flash, redirect, url_for, request
 #-*- coding: utf-8 -*-
 from app import app
 import sqlalchemy as sa
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
 from urllib.parse import urlsplit
 from app import db
@@ -31,22 +31,35 @@ def index():
     return  render_template('index.html', title ='Home Page', posts = posts)
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if current_user.is_authenticated:
+    if current_user.is_authenticated:#Если пользователь авторизован, возвращаем index ссылку
         return redirect(url_for('index'))
     form = LoginForm()
-    if form.validate_on_submit():
+    if form.validate_on_submit():#Если пользователь отправил запрос POST метода и все данные прошли проверку
         user = db.session.scalar(
-            sa.select(User).where(User.username == form.username.data))
-        if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
-            return redirect(url_for('login'))
-        login_user(user, remember=form.remember_me.data)
-        next_page = request.args.get('next')
+            sa.select(User).where(User.username == form.username.data))# запрос к бд
+        if user is None or not user.check_password(form.password.data):#проверка на правильность ввода
+            flash('Invalid username or password')# отображение сообщения при помощи функции flesh(отображает один раз)
+            return redirect(url_for('login'))# возвращение на страницу входа
+        login_user(user, remember=form.remember_me.data)#Если всё в порядке, добавляем куки файл, если нажата кнопка запомнить
+        next_page = request.args.get('next')#Получаем страницу, на которую пользователь хотел перейти
         if not next_page or urlsplit(next_page).netloc != '':
             next_page = url_for('index')
             return redirect(next_page)
-        return redirect(url_for('index'))
+        return redirect(next_page)
     return render_template('login.html', title ='Sign In', form=form)
+@app.route('/register', methods = ['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username = form.username.data, email = form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Registration successful')
+        return redirect(url_for('login'))
+    return render_template('register.html', title= 'Register', form=form)
 @app.route('/logout', methods=['GET', 'POST'])
 def logout():
     logout_user()
